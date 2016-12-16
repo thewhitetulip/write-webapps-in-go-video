7fb911822f40a49baf52dd6b8dbb7bfdc4e7255f
Suraj Patil <sapatil@live.com>
2016-12-16 23:41:09 +0530

    populating /completed url + message of no tasks

--- main.go
+++ main.go
@@ -10,9 +10,33 @@ import (
 var templates *template.Template
 var homeTemplate *template.Template
 
+type Comment struct {
+	ID      string
+	Content string
+	Author  string
+	Created string
+}
+
+type Task struct {
+	ID       string
+	Title    string
+	Content  string
+	Created  string
+	Comments []Comment
+}
+
+type Context struct {
+	Tasks []Task
+}
+
+var comments []Comment
+var tasks []Task
+
 func main() {
 	PopulateTemplate()
+
 	http.Handle("/static/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/completed/", ShowCompletedTasksFunc)
 	http.HandleFunc("/login/", LoginFunc)
 	http.HandleFunc("/", HomeFunc)
 	fmt.Println("Server running on 8081")
@@ -32,40 +56,40 @@ func PopulateTemplate() {
 	homeTemplate = templates.Lookup("tasks.html")
 }
 
+// ShowCompletedTasksFunc populates the /completed URL for the GET request for the currently loggedin user.
+func ShowCompletedTasksFunc(w http.ResponseWriter, r *http.Request) {
+	comment := Comment{ID: "1", Content: "This is a comment!", Author: "@thewhitetulip", Created: "16 Dec 2016"}
+	comment1 := Comment{ID: "2", Content: "This is a comment!", Author: "@thewhitetulip", Created: "16 Dec 2016"}
+
+	comments = append(comments, comment)
+	comments = append(comments, comment1)
+
+	task1 := Task{ID: "1", Title: "Title of First Task", Content: "Content of first task", Created: "16 Dec 2016", Comments: comments}
+
+	task2 := Task{ID: "2", Title: "Title of second Task", Content: "Content of second task", Created: "16 Dec 2016", Comments: comments}
+	task3 := Task{ID: "3", Title: "Title of third Task", Content: "Content of third task", Created: "16 Dec 2016", Comments: comments}
+
+	tasks = append(tasks, task1)
+	tasks = append(tasks, task2)
+	tasks = append(tasks, task3)
+
+	context := Context{Tasks: tasks}
+
+	homeTemplate.Execute(w, context)
+}
+
 // HomeFunc handles the / URL and asks the name of the user in German.
 func HomeFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		type Comment struct {
-			ID      string
-			Content string
-			Author  string
-			Created string
-		}
-
-		type Task struct {
-			ID       string
-			Title    string
-			Content  string
-			Created  string
-			Comments []Comment
-		}
-
-		type Context struct {
-			Tasks []Task
-		}
-
 		comment := Comment{ID: "1", Content: "This is a comment!", Author: "@thewhitetulip", Created: "16 Dec 2016"}
 		comment1 := Comment{ID: "2", Content: "This is a comment!", Author: "@thewhitetulip", Created: "16 Dec 2016"}
 
-		var comments []Comment
 		comments = append(comments, comment)
 		comments = append(comments, comment1)
 
 		task1 := Task{ID: "1", Title: "Title of First Task", Content: "Content of first task", Created: "16 Dec 2016", Comments: comments}
 		task2 := Task{ID: "2", Title: "Title of second Task", Content: "Content of second task", Created: "16 Dec 2016", Comments: comments}
 		task3 := Task{ID: "3", Title: "Title of third Task", Content: "Content of third task", Created: "16 Dec 2016", Comments: comments}
-
-		var tasks []Task
 
 		tasks = append(tasks, task1)
 		tasks = append(tasks, task2)
