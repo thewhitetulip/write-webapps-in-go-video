3d7a7525a53a1f05daf59b264b0d40750168bde0
Suraj Patil <sapatil@live.com>
2016-12-16 21:05:51 +0530

    rendering tasks.html template on GET /

--- main.go
+++ main.go
@@ -2,22 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 )
 
+var templates *template.Template
+var homeTemplate *template.Template
+
 func main() {
-	http.HandleFunc("/profile", ProfileFunc)
-	http.HandleFunc("/", HomeFuncß)
+	PopulateTemplate()
+
+	http.HandleFunc("/login/", LoginFunc)
+	http.HandleFunc("/", HomeFunc)
 	fmt.Println("Server running on 8081")
 	http.ListenAndServe("0.0.0.0:8081", nil)
 }
 
-// HomeFuncß handles the / URL and asks the name of the user in German.
-func HomeFuncß(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hallo Wie heißen Sie?")
+// PopulateTemplate reads the ./templates folder and parses all the html files inside it
+// and it stores it in the templates variable which will be lookedup by other variables.
+func PopulateTemplate() {
+	templates, err := template.ParseGlob("./templates/*.html")
+
+	if err != nil {
+		fmt.Println("main.go: PopulateTemplate: ", err)
+		os.Exit(1)
+	}
+
+	homeTemplate = templates.Lookup("tasks.html")
 }
 
-// ProfileFunc handles the /profile URL and shows the profile page of the logged in user.
-func ProfileFunc(w http.ResponseWriter, r *http.Request) {
+// HomeFunc handles the / URL and asks the name of the user in German.
+func HomeFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		homeTemplate.Execute(w, nil)
+	}
+}
+
+// LoginFunc handles the /login URL and shows the profile page of the logged in user on a GET request
+// handles the Login process on the POST request.
+func LoginFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "You are on the profile page!!")
 }
